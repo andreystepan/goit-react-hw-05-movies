2cd47e3bff58e9518d770ede50a0ac47b688b90e
andreystepan <92333697+andreystepan@users.noreply.github.com>
2022-09-09 18:34:08 +0300

    correction

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -8,6 +8,7 @@ const MovieDetails = () => {
   const { movieId } = useParams();
   const [movieInfo, setMovieInfo] = useState(null);
   const location = useLocation();
+  // const navigate = useNavigate();
 
   useEffect(() => {
     getByIdMovies(movieId).then(setMovieInfo);
@@ -40,9 +41,13 @@ const MovieDetails = () => {
         <h3>Additional information</h3>
       </Container>
       <Box>
-        <NavLink to="cast">Cast</NavLink>
+        <NavLink to="cast" state={location.state}>
+          Cast
+        </NavLink>
 
-        <NavLink to="reviews">Reviews</NavLink>
+        <NavLink to="reviews" state={location.state}>
+          Reviews
+        </NavLink>
       </Box>
 
       <Outlet />
